7dddbcb2f4a4f3828265831057b9f73db11dac54
Jaime-Ginesky <jaimelee79@gmail.com>
2023-05-02 23:54:42 -0700

    optimized code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 1,
+      title: 'Jackets'
+    },
+    {
+      id: 1,
+      title: 'Shoes'
+    },
+    {
+      id: 1,
+      title: 'Womens'
+    },
+    {
+      id: 1,
+      title: 'Mens'
+    }
+  ]
   return (
     
     <div className="categories-container">
+      {categories.map(({title}) => (
       <div className="category-container">
         <div className="category-body-container">
-          <h2>Hats</h2>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+       ))}
     </div>
   );
-}
+      }
 
 export default App;
