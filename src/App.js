0f557b67c69c475aaa81392daeb461692d678492
Jaime-Ginesky <jaimelee79@gmail.com>
2023-05-02 23:39:16 -0700

    added categories

--- src/App.js
+++ src/App.js
@@ -3,8 +3,37 @@
 const App = () => {
   return (
     
-    <div>
-      Hello World
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
