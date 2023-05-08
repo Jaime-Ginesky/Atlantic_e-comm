d67110aa77154265646322d06d4ea158127b7b61
Jaime-Ginesky <jaimelee79@gmail.com>
2023-05-07 22:35:11 -0700

    created links to navigation elements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './routes/navigation/navigation.component';
 
 import Home from './routes/home/home.component';
 
-const Navigation = () => {
-  return(
-    <div>
-      <div>
-        <h1>Navigation Bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const Shop = () => {
   return <h1>Shop Page</h1>; 
