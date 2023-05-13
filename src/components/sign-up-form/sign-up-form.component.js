04b0d76eb089e731fbcd2c4238a8261fc94df9fd
Jaime-Ginesky <jaimelee79@gmail.com>
2023-05-13 14:56:16 -0700

    authentication for handlechange and handle submit, reset default form

--- src/components/sign-up-form/sign-up-form.component.js
+++ src/components/sign-up-form/sign-up-form.component.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { createUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
     displayName: '',
@@ -14,6 +14,31 @@ const SignUpForm = () => {
     const { displayName, email, password, confirmPassword } = formFields;
       console.log(formFields);
 
+      const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+      }
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+       if(password!==confirmPassword) {
+        alert("Passwords do not match");
+        return;
+       }
+       
+       try {
+        const { user} = await createAuthUserWithEmailAndPassword(email, password);
+
+        await createUserDocumentFromAuth(user,{ displayName });
+        resetFormFields();
+       } catch(error) {
+        if(error.code === 'auth/email-already-in-use') {
+            alert('Cannot create user, email already in use')
+        } else {
+        console.log('user creation encountered an error', error);
+       }
+     }  
+    };
+
     const handleChange = (event) => {
      const {name, value} = event.target;
      setFormFields({ ...formFields, [name]: value })
@@ -22,7 +47,7 @@ const SignUpForm = () => {
     return (
         <div>
             <h1>Sign up with your email and password</h1>
-            <form>
+            <form onSubmit={() => {}}>
               <label>Display Name</label>
               <input type="text" required onChange={handleChange} name='displayName' value={displayName} />
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -38,7 +38,11 @@ const firebaseConfig = {
 
   export const db = getFirestore();
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (
+    userAuth, 
+    additionalInformation = {}
+    ) => {
+        if(!userAuth) return;
 
     const userDocRef = doc(db, 'users', userAuth.uid); 
 
@@ -53,6 +57,7 @@ const firebaseConfig = {
             displayName,
             email,
             createdAt,
+            ...additionalInformation
         });
     } catch(error) {
         console.log('error creating the user', error.message);
