3713a1a106616f75027daa02dec56c765ab96d31
Jaime-Ginesky <jaimelee79@gmail.com>
2023-05-14 18:49:24 -0700

    added sign in component and authentication route

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -13,10 +13,10 @@ const Navigation = () => {
               <CrwnLogo className='logo' />
             </Link>
             <div className='nav-links-container'>
-                <Link className='nav=link' to='/shop'>
+                <Link className='nav-link' to='/shop'>
                     SHOP
                 </Link>
-                <Link className='sign-in' to='/sign-in'>
+                <Link className='nav-link' to='/auth'>
                     SIGN IN
                 </Link>
             </div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import { getAuth,
 signInWithRedirect,
 signInWithPopup,
 GoogleAuthProvider,
-createUserWithEmailAndPassword
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -70,4 +71,10 @@ const firebaseConfig = {
     if(!email || !password) return;
 
      return await createUserWithEmailAndPassword(auth,email,password);
+  };
+
+  export const signInAuthUserWithEmailAndPassword = async (email,password) => {
+    if(!email || !password) return;
+
+     return await signInWithEmailAndPassword(auth,email,password);
   };
