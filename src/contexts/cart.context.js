39cddc03959e6501d7a222a6059212a61721ac9d
Jaime-Ginesky <jaimelee79@gmail.com>
2023-05-23 23:55:49 -0700

    add product to cart

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -2,7 +2,16 @@ import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
 
-}
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id===productToAdd.id);
+
+    if(existingCartItem) {
+        return cartItems.map(() => cartItems.id ===productToAdd.id ?
+        {...cartItems, quantity: cartItems.quantity + 1 }
+        : cartItems
+        );
+    }
+    return [...cartItems, {...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
     isCartOpen: false,
@@ -18,7 +27,7 @@ export const CartProvider = ({ children }) => {
     const addItemToCart = (productToAdd) => {
 setCartItems(addCartItem(cartItems, productToAdd));
     }
-    const value = { isCartOpen, setIsCartOpen };
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
