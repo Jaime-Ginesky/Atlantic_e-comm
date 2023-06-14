3de3e5c7d6d89b95cc0423744f51443ce862e27e
Jaime-Ginesky <jaimelee79@gmail.com>
2023-06-13 22:58:06 -0700

    added checkout component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Authentication from './routes/authentication/authentication.component';
 import Home from './routes/home/home.component';
 
 import Shop from './routes/shop/shop.component';
-
+import Checkout from './routes/checkout/checkout.component';
 
 
 const App = () => {
@@ -16,6 +16,7 @@ const App = () => {
       <Route index element={<Home />} />
       <Route path='shop' element={<Shop />} />
       <Route path='auth' element={<Authentication />} />
+      <Route path='auth' element={<Checkout />} />
     </Route>
    </Routes>
    );
---
+++ src/routes/checkout/checkout.component.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
+import "./checkout.styles.scss";
+import CartItem from "../../components/cart-item/cart-item.component";
+
+
+const Checkout = () => {
+    const { cartItems } = useContext(CartContext);
+    
+    return (
+        <div>
+            <h1>I am the checkout page</h1>
+        
+           {cartItems.map((CartItem) => {
+                const { id, name, quantity } = CartItem;
+                return (
+                <div key={id}>
+                <h2>{name}</h2>
+                <span>{quantity}</span>
+              </div>
+            );
+        })}
+</div>
+
+export default Checkout;
