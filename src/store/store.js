bd5d24fb9af887fe3e98bf734832e0d50bb537ed
Jaime-Ginesky <jaimelee79@gmail.com>
2023-07-06 23:41:25 -0700

    created userReducer

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,8 @@ import logger from 'redux-logger';
 
 import { RootReducer } from './rootreducer';
 
-const middleWares = { logger }
+const middleWares = [logger]
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const Store = legacy_createStore( RootReducer, undefined, middleWares)
+export const store = legacy_createStore( RootReducer, undefined, composedEnhancers)
