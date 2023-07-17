4c3ed79893e2ff69a0fb6e5bf1fc4444eb16fb5d
Jaime-Ginesky <jaimelee79@gmail.com>
2023-07-16 22:09:59 -0700

    installed redux-thunk and restructured categories

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, legacy_createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './rootreducer';
 
@@ -15,7 +16,8 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middlewares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
+
 
 const composeEnhancers = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
 compose;
