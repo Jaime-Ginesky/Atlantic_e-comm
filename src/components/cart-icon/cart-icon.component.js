36b9996fdd93650d44dbdb6780bfabbb7644a88d
Jaime-Ginesky <jaimelee79@gmail.com>
2024-01-12 23:34:14 -0800

    changed shopping icon and styled

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -1,12 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
-
 import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
 import { setIsCartOpen } from '../../store/cart/cart.action';
 
-// import { useContext } from 'react';
-// import { CartContext } from '../../contexts/cart.context.js';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from '../../assets/bag-shopping.svg';
 import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 
